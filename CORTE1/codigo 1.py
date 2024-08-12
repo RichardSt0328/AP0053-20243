2c1b1bf1aa9e69f6e9d2cc3f44f8a6b4750924dc
RichardSt0328 <79875719+RichardSt0328@users.noreply.github.com>
2024-08-12 07:49:02 -0500

    Add files via upload

---
+++ CORTE1/codigo 1.py
@@ -0,0 +1,7 @@
+print("mi primera vez con python")
+a = "cadena de caracteres"
+print("tipo dato a: ", type (a))
+b = 8
+print("tipo dato b: ", type (b))
+c = 3.1415
+print("tipo dato c: ", type (c))
